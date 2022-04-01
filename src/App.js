c084fc5dfa8f2fec0770821b3d65e81b0343cad0
Akshay Tondak <akshaytondak@Akshays-MacBook-Air.local>
2022-04-01 12:29:39 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -12,15 +12,53 @@ const decodeFileBase64 = (base64String) => {
   );
 };
 
+function convertURIToBinary(dataURI) {
+  let BASE64_MARKER = ';base64,';
+  let base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+  let base64 = dataURI.substring(base64Index);
+  let raw = window.atob(base64);
+  let rawLength = raw.length;
+  let arr = new Uint8Array(new ArrayBuffer(rawLength));
+
+  for (let i = 0; i < rawLength; i++) {
+    arr[i] = raw.charCodeAt(i);
+  }
+  return arr;
+}
+const DROPDOWN_API_ENDPOINT = 'https://qyst0wsnm1.execute-api.us-east-1.amazonaws.com/prod/';
+
+
 
 function App() {
   const [inputFileData, setInputFileData] = React.useState(''); // represented as bytes data (string)
   const [outputFileData, setOutputFileData] = React.useState(''); // represented as readable data (text string)
   let [spectrogramData, setSpectrogramData] = React.useState('');
   let [barGraphData, setBarGraphData] = React.useState('');
+  const [submitButtonText, setSubmitButtonText] = React.useState('Submit');
   const [buttonDisable, setButtonDisable] = React.useState(true);
   const [audioPlayerDisable, setAudioPlayerDisable] = React.useState(true);
   const [buttonText, setButtonText] = React.useState('Submit');
+  const [demoDropdownFiles, setDemoDropdownFiles] = React.useState([]);
+  const [selectedDropdownFile, setSelectedDropdownFile] = React.useState('');
+  const [inputImage, setInputImage] = React.useState(''); // represented as bytes data (string)
+
+  React.useEffect(() => {
+    fetch(DROPDOWN_API_ENDPOINT)
+    .then(response => response.json())
+    .then(data => {
+      // GET request error
+      if (data.statusCode === 400) {
+        console.log('Sorry! There was an error, the demo files are currently unavailable.');
+      }
+      
+      // GET request success
+      else {
+        console.log("success");
+        const s3BucketFiles = JSON.parse(data.body);
+        setDemoDropdownFiles(s3BucketFiles["s3Files"]);
+      }
+    });
+  }, [])
 
   // convert file to bytes data
   const convertFileToBytes = (inputFile) => {
@@ -99,6 +137,57 @@ function App() {
     setButtonDisable(false);
     Show_Audio();
 
+    setSelectedDropdownFile('');
+
+  }
+
+  // handle demo dropdown file selection
+  const handleDropdown = async (event) => {
+    setSelectedDropdownFile(event.target.value);
+
+    // temporarily disable submit button
+    setButtonDisable(true);
+    setSubmitButtonText('Loading Demo File...');
+
+    // only make POST request on file selection
+    if (event.target.value) {
+      fetch(DROPDOWN_API_ENDPOINT, {
+        method: 'POST',
+        body: JSON.stringify({ "fileName": event.target.value })
+      }).then(response => response.json())
+      .then(data => {
+
+        // POST request error
+        if (data.statusCode === 400) {
+          console.log('Uh oh! There was an error retrieving the dropdown file from the S3 bucket.')
+        }
+
+        // POST request success
+        else {
+          console.log("demo file post req success")
+          const dropdownFileBytesData = JSON.parse(data.body)['bytesData'];
+          setOutputFileData("");
+          setSpectrogramData("");
+          setBarGraphData("");
+      
+          //show audio player
+          var sound = document.getElementById('audio');
+          sound.src = "data:audio/ogg;base64,".concat(dropdownFileBytesData);
+          sound.onend = function(e) {
+            URL.revokeObjectURL(this.src);
+          }
+      
+          setInputFileData(dropdownFileBytesData);
+      
+          // enable submit button
+          setButtonDisable(false);
+          Show_Audio();
+        }
+      });
+    }
+    else {
+      setInputFileData('');
+    }
   }
 
   function Show_Results() {
@@ -172,6 +261,11 @@ function App() {
         <form onSubmit={handleSubmit}>
           <br/>
           <input class="custom-file-input" type="file" accept=".wav" onChange={handleChange} />
+          <label htmlFor="demo-dropdown">Demo: </label>
+          <select class="custom-file-input" name="Select Audio" id="demo-dropdown" value={selectedDropdownFile} onChange={handleDropdown}>
+            <option value="">-- Select Demo File --</option>
+            {demoDropdownFiles.map((file) => <option key={file} value={file}>{file}</option>)}
+          </select>
           <br/><br/>
           <div id="audioplayer" class="Output">
             Here is your audio :<br/><br/>
